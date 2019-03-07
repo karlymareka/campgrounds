84f28db932fcd381a5ffe59572e43461935e8581
Karly Mareka <karly.mareka@gmail.com>
2019-03-07 22:37:29 +0000

    first commit

---
+++ lib/campgrounds.rb
@@ -0,0 +1,6 @@
+require "campgrounds/version"
+
+module Campgrounds
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/campgrounds/version.rb
@@ -0,0 +1,3 @@
+module Campgrounds
+  VERSION = "0.1.0"
+end
